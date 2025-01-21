f2bd3f1d6adf3bd7bb33cdecd1e2edd0c41c6ee2
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2025-01-21 17:34:45 -0300

    Remove unused pages.dev.js and update build command in pages.toml; add gunicorn to requirements.txt

--- pages.dev.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname.startsWith('/static/')) {
-      return env.ASSETS.fetch(request);
-    }
-    return env.FLASK.fetch(request);
-  }
-}
